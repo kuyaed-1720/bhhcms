1c2f501e8b5ccac5ead9fb026cbe8ee8803c4956
atelier-eureka <ateliereureka1720@gmail.com>
2025-03-04 13:57:46 +0800

    Initial commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,145 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}" x-data="app()" x-cloak>
+        @csrf
+
+        <div x-show.transition="step === 'complete'">
+            <div class="bg-white rounded-lg p-10 flex items-center shadow justify-between">
+                <div>
+                    <svg class="mb-4 h-20 w-20 text-green-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                    </svg>
+
+                    <h2 class="text-2xl mb-4 text-gray-800 text-center font-bold">Registration Success</h2>
+
+                    <div class="text-gray-600 mb-8">
+                        Thank you. We have sent you an email to demo@demo.test. Please click the link in the message to activate your account.
+                    </div>
+
+                    <button
+                        @click="step = 1"
+                        class="w-40 block mx-auto focus:outline-none py-2 px-5 rounded-lg shadow-sm text-center text-gray-600 bg-white hover:bg-gray-100 font-medium border">Back to home</button>
+                </div>
+            </div>
+        </div>
+
+        <div x-show-transition="step != 'complete'">
+
+            <!-- Full Name -->
+            <div x-show.transition.in="step === 1">
+                <!-- First Name -->
+                <div>
+                    <x-input-label for="first_name" :value="__('First Name')" />
+                    <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+                </div>
+
+                <!-- Middle Name -->
+                <div>
+                    <x-input-label for="middle_name" :value="__('Middle Name')" />
+                    <x-text-input id="middle_name" class="block mt-1 w-full" type="text" name="middle_name" :value="old('middle_name')" required autocomplete="name" />
+                    <x-input-error :messages="$errors->get('middle_name')" class="mt-2" />
+                </div>
+
+                <!-- Last Name -->
+                <div>
+                    <x-input-label for="last_name" :value="__('Name')" />
+                    <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autocomplete="name" />
+                    <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+                </div>
+            </div>
+
+            <!-- Contact Information and Designation-->
+            <div x-show.transition.in="step === 2">
+                <!-- Phone Number -->
+                <div>
+                    <x-input-label for="phone" :value="__('Phone Number')" />
+                    <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required autocomplete="tel" />
+                    <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+                </div>
+
+                <!-- Address -->
+                <div>
+                    <x-input-label for="purok" :value="__('Purok')" />
+                    <x-text-input id="purok" class="block mt-1 w-full" type="text" name="purok" :value="old('purok')" required autocomplete="name" />
+                    <x-input-error :messages="$errors->get('purok')" class="mt-2" />
+                </div>
+            </div>
+
+            <!-- Login Credentials -->
+            <div x-show.transition.in="step === 3">
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')" />
+
+                    <x-text-input id="password" class="block mt-1 w-full"
+                        type="password"
+                        name="password"
+                        required autocomplete="new-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                        type="password"
+                        name="password_confirmation" required autocomplete="new-password" />
+
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+                <button
+                    x-show="step > 1"
+                    @click="step--"
+            class="ms-4">Previous</button>
+                <button
+                    x-show="step < 3"
+                    @click="step++"
+class="ms-4">Next"</button>                <x-primary-button x-show="step > 2" class="ms-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+        </div>
+    </form>
+    <script>
+        function app() {
+            return {
+                step: 1,
+                passwordStrengthText: '',
+                togglePassword: false,
+               image: ''
+                password: '',
+                gender: 'Male',
+
+                checkPasswordStrength() {
+                    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+                    var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+                    let value = this.password;
+
+                    if (strongRegex.test(value)) {
+                        this.passwordStrengthText = "Strong password";
+                    } else if (mediumRegex.test(value)) {
+                        this.passwordStrengthText = "Could be stronger";
+                    } else {
+                        this.passwordStrengthText = "Too weak";
+                    }
+                }
+            }
+        }
+    </script>
+</x-guest-layout>
